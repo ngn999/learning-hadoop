934b438a1fbf6c3e0837b65b2578e1b896f9a67b
tangyi <ngn998@gmail.com>
2013-09-25 14:17:51 +0800

    do DOUBLE cat

--- src/FileSystemDoubleCat.java
+++ src/FileSystemDoubleCat.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.io.IOUtils;
 
 public class FileSystemDoubleCat {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String uri = args[0];
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(uri), conf);
@@ -16,7 +16,7 @@ public class FileSystemDoubleCat {
             in = fs.open(new Path(uri));
             IOUtils.copyBytes(in, System.out, 4096, false);
             in.seek(0);
-            // IOUtils.copyBytes(in, System.out, 4096, false);
+            IOUtils.copyBytes(in, System.out, 4096, false);
         } finally {
             IOUtils.closeStream(in);
         }
